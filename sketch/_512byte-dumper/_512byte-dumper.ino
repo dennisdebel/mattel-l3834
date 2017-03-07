f23ca526f8e002a7e0e34852c1109a655350b8f2
dennisdebel <dennisdebel@gmail.com>
2017-03-07 18:46:47 +0100

    added reader sketch and memdump

---
+++ sketch/_512byte-dumper/_512byte-dumper.ino
@@ -0,0 +1,103 @@
+/**************************************************************************//**
+ * \brief EEPROM 24C04 / 24C16 library for Arduino - Demonstration program
+ * \author Copyright (C) 2012  Julien Le Sech - www.idreammicro.com
+ * \version 1.0
+ * \date 20120218
+ *
+ * This file is part of the EEPROM 24C04 / 24C16 library for Arduino.
+ *
+ * This library is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/
+ ******************************************************************************/
+
+/**************************************************************************//**
+ * \file WriteReadByte.ino
+ ******************************************************************************/
+ 
+/******************************************************************************
+ * Header file inclusions.
+ ******************************************************************************/
+
+#include <Wire.h>
+
+#include <Eeprom24C04_16.h>
+
+/******************************************************************************
+ * Private macro definitions.
+ ******************************************************************************/
+
+/**************************************************************************//**
+ * \def EEPROM_ADDRESS
+ * \brief Address of EEPROM memory on TWI bus.
+ ******************************************************************************/
+#define EEPROM_ADDRESS  0x50
+
+/******************************************************************************
+ * Private variable definitions.
+ ******************************************************************************/
+
+static Eeprom24C04_16 eeprom(EEPROM_ADDRESS);
+
+/******************************************************************************
+ * Public function definitions.
+ ******************************************************************************/
+
+/**************************************************************************//**
+ * \fn void setup()
+ *
+ * \brief
+ ******************************************************************************/
+void setup()
+{
+    // Initialize serial communication.
+    Serial.begin(9600);
+        
+    // Initialize EEPROM library.
+    eeprom.initialize();
+    
+    // Write a byte at address 0 in EEPROM memory.
+    //Serial.println("Write byte to EEPROM memory...");
+    //eeprom.writeByte(0, 0xAA);
+    
+    // Write cycle time (tWR). See EEPROM memory datasheet for more details.
+    //delay(10);
+    
+
+}
+
+/**************************************************************************//**
+ * \fn void loop()
+ *
+ * \brief
+ ******************************************************************************/
+void loop()
+{
+
+    // Read a byte at address 0 in EEPROM memory.
+    Serial.println("Read byte from EEPROM memory...");
+    for (int i=0; i<512; i++) {
+      byte data = eeprom.readByte(i);
+      
+      if (!i%32) 
+          Serial.println("");
+
+    // Print read byte.
+      Serial.print("0x");
+      Serial.print(data, HEX);
+      Serial.print(" ");
+
+    }
+    Serial.println("");
+
+    delay(500);
+}
